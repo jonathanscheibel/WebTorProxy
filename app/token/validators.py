c3246914909cd4b2b6a5c9f0f4c43883a514c913
jonathan <jonathansmorais@gmail.com>
2021-08-19 21:53:40 -0300

    Melhoria em validaçao de utilização da API

--- app/token/validators.py
+++ app/token/validators.py
@@ -10,17 +10,27 @@ def is_running(script):
     return False
 
 
+def check_call_valid():
+    word = 'if not validate(token):'
+    with open(os.getcwd() + '/app/routes.py') as f:
+        for l_num, l in enumerate(f, 1):
+            if word in l:
+                if '#' not in l.strip():
+                    return True
+        return False
+
+
 def validate(token):
-    if is_running("check_licence.py"):
-        if is_valid(token):
-            return True
+    if check_call_valid():
+        if is_running("check_licence.py"):
+            if is_valid(token):
+                return True
     return False
 
 
 def is_valid(token):
-    return token == '7bdb3adea2e032b0e2e31116f48080a3' # TODO mudar para consulta no banco
+    return token == '7bdb3adea2e032b0e2e31116f48080a3'  # TODO mudar para consulta no banco
 
 
 def not_valid():
     return {"success": False}, 401
-
